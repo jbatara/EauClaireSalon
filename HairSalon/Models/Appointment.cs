f6dbbea52212a1ad27fa8ff2765e591c9dcd0e1a
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-18 15:11:34 -0700

    Change ApptID field name to AppointmentID so Entity can implicity bind the primary key to that variable

--- HairSalon/Models/Appointment.cs
+++ HairSalon/Models/Appointment.cs
@@ -4,7 +4,7 @@ namespace Salon.Models
 {
     public class Appointment
     {
-        public int ApptID {get;set;}
+        public int AppointmentID {get;set;}
         public DateTime ApptDateTime {get;set;}
         public bool Paid {get;set;}
         public int ServiceID {get;set;}
