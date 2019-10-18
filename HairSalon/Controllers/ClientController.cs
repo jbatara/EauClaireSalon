bb156a3db087c0967f48d3cddc6388d9e9e7ec65
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-18 15:13:48 -0700

    Add view for a list of all clients and adding new client.

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -15,22 +15,42 @@ namespace Salon.Controllers
       _db = db;
     }
 
-    [HttpGet("/")]
+    [HttpGet("/client")]
     public ActionResult Index()
     {
+      ViewBag.clients = _db.Clients.OrderBy(c => c.LastName).ToList();
+      ViewBag.stylists = _db.Stylists.OrderBy(s => s.StylistID).ToList();
       return View();
     }
 
+    [HttpGet("/client/new")]
     public ActionResult New()
     {
-      List<Stylist> sList = _db.Stylists.ToList();
+      List<Stylist> sList = _db.Stylists.OrderBy(s => s.LastName).ToList();
       List<SelectListItem> dropDownList = new List<SelectListItem>();
       foreach (Stylist s in sList)
       {
-        SelectListItem item = new SelectListItem() { Text = "${s.FirstName} {s.LastName}", Value = $"{ s.StylistID }" };
+        SelectListItem item = new SelectListItem() { Text = $"{s.FirstName} {s.LastName}", Value = $"{ s.StylistID }" };
         dropDownList.Add(item);
       }
       ViewBag.dropDownList = dropDownList;
+      return View();
+    }
+
+    public ActionResult New(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      int id = client.ClientID;
+      int stylistID = client.StylistID;
+
+      return Redirect($"/stylist/{stylistID}/client/{id}");
+    }
+
+    [HttpGet("/stylist/{stylistID}/client/{id}")]
+    public ActionResult Client(int stylistID, int id)
+    {
+      ViewBag.client = _db.Clients.FirstOrDefault(c => c.ClientID == id) as Client;
       return View();
     }
   }
