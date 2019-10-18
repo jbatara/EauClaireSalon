a55d6b02771afc7f4eb57b451e88f59294b9fee4
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-18 12:26:08 -0700

    Add Title function for stylist class to return stylist title

--- HairSalon/Models/Sytlists.cs
+++ HairSalon/Models/Sytlists.cs
@@ -3,7 +3,26 @@ namespace Salon.Models
     public class Stylist
     {
         public int StylistID { get; set; }
+        public int StylistTier {get;set;}
         public string Name {get;set;}
+
+        public string Title()
+        {
+            if(StylistTier == 3)
+            {
+                return "Master Stylist";
+            }
+            else if (StylistTier == 2)
+            {
+                return "Stylist";
+            }
+            else
+            {
+                return "Apprentice";
+            }
+        }
         
     }
+
+
 }
