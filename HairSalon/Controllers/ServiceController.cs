97bb8b93feed5ecb8fadf85de9863941c0249070
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-18 15:55:17 -0700

    Add /service table view and add service form content and routes

--- HairSalon/Controllers/ServiceController.cs
+++ HairSalon/Controllers/ServiceController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using Salon.Models;
 
 namespace Salon.Controllers
@@ -12,11 +13,24 @@ namespace Salon.Controllers
       _db = db;
     }
 
-
+    [HttpGet("/service")]
     public ActionResult Index()
     {
-
+      ViewBag.services = _db.Services.OrderBy(s => s.ServiceID).ToList();
       return View();
+    }
+
+    [HttpGet("/service/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    public ActionResult New(Service service)
+    {
+      _db.Services.Add(service);
+      _db.SaveChanges();
+      return Redirect("/service");
     }
   }
 }
