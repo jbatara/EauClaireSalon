d6ffd09a43b4b0ade62c0fef011a154ad21e0cd5
Student <example@example.com>
2019-10-21 16:14:41 -0700

    Add design context and salon context to be able to create database before run

---
+++ HairSalon/Migrations/20191021225908_Initial.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Salon.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ApptDateTime = table.Column<DateTime>(nullable: false),
+                    Paid = table.Column<bool>(nullable: false),
+                    ServiceID = table.Column<int>(nullable: false),
+                    ClientID = table.Column<int>(nullable: false),
+                    StylistID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.AppointmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    StylistID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    ServiceID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Tier1Price = table.Column<double>(nullable: false),
+                    Tier2Price = table.Column<double>(nullable: false),
+                    Tier3Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.ServiceID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stylists",
+                columns: table => new
+                {
+                    StylistID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StylistTier = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stylists", x => x.StylistID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Stylists");
+        }
+    }
+}
--- HairSalon/Models/SalonContext.cs
+++ HairSalon/Models/SalonContext.cs
@@ -4,7 +4,7 @@ namespace Salon.Models
 {
   public class SalonContext : DbContext
   {
-    public virtual DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Stylist> Stylists { get; set; }
     public virtual DbSet<Client> Clients { get; set; }
     public virtual DbSet<Service> Services {get;set;}
     public virtual DbSet<Appointment> Appointments {get;set;}
