e382c951cfdb76f5dca5febd546ab213d91ba652
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-10-29 23:59:00 -0700

    Add join table for appointments and services

---
+++ HairSalon/Models/AppointmentService.cs
@@ -0,0 +1,9 @@
+namespace Salon.Models
+{
+    public class AppointmentService
+    {
+        public int AppointmentServiceID {get;set;}
+        public int AppointmentID {get;set;}
+        public int ServiceID {get;set;}
+    }
+}
